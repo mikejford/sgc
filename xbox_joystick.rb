6d3dd58c8679861e965a1e96f601a6b8a0010343
Mike Ford <mikejford@gmail.com>
2013-11-08 21:44:53 -0600

    Troubleshooting a ThreadError

--- xbox_joystick.rb
+++ xbox_joystick.rb
@@ -15,32 +15,36 @@ class XboxJoystick < Artoo::Robot
   end
 
   work do
+    on controller, :button_rb => proc { |*value|
+      # turn on sphero back led
+      sphero_bot.calibration_led(0xff)
+    }
     on controller, :button_up_rb => proc { |*value|
       # turn off sphero back led
       sphero_bot.calibration_led(0x00)
     }
-    on controller, :joystick_0 => proc { |*value|
-      x = value[1][:x]
-      y = value[1][:y]
-
-      calibrating = controller.button_values[controller.button_map.key(:rb)]
-
-      r = speed_value x, y
-      theta = heading_value x, y
-
-      if calibrating == 1
-        # turn on sphero back led
-        sphero_bot.calibration_led(0xff)
-        sphero_bot.calibrate(theta)
-      else
-        if r > 0
-          sphero_bot.roll(r, theta)
-        end
-      end
-    }
+    #on controller, :joystick_0 => :joystick_action
   end
 
   private 
+
+  def joystick_action *value
+    x = value[1][:x]
+    y = value[1][:y]
+
+    calibrating = controller.button_values[controller.button_map.key(:rb)]
+
+    r = speed_value x, y
+    theta = heading_value x, y
+
+    if calibrating == 1
+      sphero_bot.calibrate(theta)
+    else
+      if r > 0
+        sphero_bot.roll(r, theta)
+      end
+    end
+  end
 
   def speed_value x, y
     num = Math.sqrt(x**2 + y**2)
