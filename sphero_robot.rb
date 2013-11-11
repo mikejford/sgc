adf18d4a5aff6b7616956b9337a089ed6a42ccea
Mike Ford <mikejford@gmail.com>
2013-11-11 14:43:02 -0600

    Add mutex support for commands

--- sphero_robot.rb
+++ sphero_robot.rb
@@ -5,17 +5,38 @@ class SpheroRobot < Artoo::Robot
   connection :sphero, :adaptor => :sphero, :port => '127.0.0.1:4560'
   device :sphero, :driver => :sphero
 
+  def initialize(params={})
+    @lock = Mutex.new
+    super params
+  end
+
   def calibration_led(led_brightness)
-    sphero.back_led_output = led_brightness
+    do_action do
+      puts "Sphero brightness #{led_brightness}"
+      sphero.back_led_output = led_brightness
+    end
   end
 
   def calibrate(heading)
-    sphero.heading = heading
+    do_action do
+      puts "Sphero heading #{heading}"
+      sphero.heading = heading
+    end
   end
 
   def roll(speed, heading)
-    sphero.roll(speed, heading)
+    do_action do
+      sphero.roll(speed, heading)
+    end
   end
 
+  private
+
+  def do_action
+    if @lock.try_lock
+      yield
+      @lock.unlock
+    end
+  end
 end
 
