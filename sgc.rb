755f6e7ee5b0f45bdef502fdd2a134259165c0f8
Mike Ford <mikejford@gmail.com>
2013-12-04 00:26:03 -0600

    Rename from sxb to sgc
    Rename reflects the change to support PS3 controller along with XBox360
    controller

--- sgc.rb
+++ sgc.rb
@@ -4,7 +4,7 @@ require_relative 'command_queue'
 
 cmd_q = CommandQueue.new()
 
-cjs = ControllerJoystick::XBox360.new({ :command_queue => cmd_q })
+cjs = ControllerJoystick::PS3.new({ :command_queue => cmd_q })
 
 sphero_bot = SpheroRobot.new({ :connections => { :sphero => { :port => '127.0.0.1:4560' } },
                                :command_queue => cmd_q })
